9b3ba87f43815600cf67500065920f32106149c8
Mikael Janhonen <janhon3n@gmail.com>
2018-04-17 10:15:20 +0300

    Update GoHttpServer.hs

--- src/GoHttpServer.hs
+++ src/GoHttpServer.hs
@@ -1,13 +1,21 @@
 module GoHttpServer where
-{-
-import Happstack.Lite
+
+import Happstack.Server
 import Text.Blaze.Html5 (Html, (!), a, form, input, p, toHtml, label)
 import Text.Blaze.Html5.Attributes (action, enctype, href, name, size, type_, value)
 import Text.Blaze.Html5 as H
 import Text.Blaze.Html5.Attributes as A
 
-server :: ServerPart Response
-server = path $ \(msg :: String) ->
-   ok $ template "Haskell go" $ do
-      H.h1 "Haskell GO"
-      H.p $ a ! href "/datajotain" -}
+conf :: Conf
+conf = Conf { port = 3008
+      , validator = Nothing
+      , logAccess = Just logMAccess
+      }
+
+startServer :: IO ()
+startServer = simpleHTTP conf $ msum [ dir "menu" $ menu
+      , dir "play" $ play
+      ]
+
+menu :: ServerPart String
+menu = path $ \s -> ok $ "Hello, " ++ s
